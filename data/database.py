745cedda676ca59817555927a941e779480e7ed0
Laughterayche <kostrominefim2@gmail.com>
2023-12-14 22:04:07 +0500

    Alex

--- data/database.py
+++ data/database.py
@@ -2,7 +2,6 @@ import psycopg2
 
 conn = psycopg2.connect(user='postgre', password='1234')
 
-print()
 class database:
     def __init__(self, name):
         self.name = name
@@ -10,21 +9,22 @@ class database:
     def create_table(self):
         with conn.cursor() as cur:
             cur.execute(f"""CREATE TABLE IF NOT EXISTS {self.name}
-            (id INT PRIMARY KEY,
-            bank INT)
+            (id INT,
+            deposit INT,
+            positions TEXT)
             ;""")
             print('[INFO] TABLE CREATED SUCCESSFULLY')
 
     def insert_new_user(self, id):
         with conn.cursor() as cur:
-            cur.execute(f"""INSERT INTO {self.name} (id, bank) 
-            VALUES ({id}, {100});
+            cur.execute(f"""INSERT INTO {self.name} (id, deposit, positions) 
+            VALUES ({id}, {100000}, '');
             """)
             print('[INFO] USER INSERT SUCCESSFULLY')
 
     def get_user_info(self, id):
         with conn.cursor() as cur:
-            cur.execute(f"""SELECT bank FROM {self.name} 
+            cur.execute(f"""SELECT deposit, positions FROM {self.name} 
             WHERE id = {id};
             """)
             print('[INFO] USER INFO DROPPED')
