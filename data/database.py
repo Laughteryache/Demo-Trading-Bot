269dbdb8cfc50acf3a00dcbc60d3f7b187d66efa
Laughterayche <kostrominefim2@gmail.com>
2023-12-14 23:17:43 +0500

    Alex

--- data/database.py
+++ data/database.py
@@ -1,30 +1,36 @@
-import psycopg2
+import sqlite3
 
-conn = psycopg2.connect(user='postgre', password='1234')
+conn = sqlite3.connect('test.py')
 
-class database:
+class Database:
     def __init__(self, name):
         self.name = name
 
     def create_table(self):
-        with conn.cursor() as cur:
-            cur.execute(f"""CREATE TABLE IF NOT EXISTS {self.name}
-            (id INT,
-            deposit INT,
-            positions TEXT)
-            ;""")
-            print('[INFO] TABLE CREATED SUCCESSFULLY')
+        cur = conn.cursor()
+        cur.execute(f"""CREATE TABLE IF NOT EXISTS {self.name}
+        (id INT,
+        deposit INT,
+        positions TEXT)
+        ;""")
+        conn.commit()
+        cur.close()
+        print('[INFO] TABLE CREATED SUCCESSFULLY')
 
     def insert_new_user(self, id):
-        with conn.cursor() as cur:
-            cur.execute(f"""INSERT INTO {self.name} (id, deposit, positions) 
-            VALUES ({id}, {100000}, '');
-            """)
-            print('[INFO] USER INSERT SUCCESSFULLY')
+        cur = conn.cursor()
+        cur.execute(f"""INSERT INTO {self.name} (id, deposit, positions) 
+        VALUES ({id}, {100000}, '');
+        """)
+        conn.commit()
+        cur.close()
+        print('[INFO] USER INSERT SUCCESSFULLY')
 
     def get_user_info(self, id):
-        with conn.cursor() as cur:
-            cur.execute(f"""SELECT deposit, positions FROM {self.name} 
-            WHERE id = {id};
-            """)
-            print('[INFO] USER INFO DROPPED')
+        cur = conn.cursor()
+        cur.execute(f"""SELECT deposit, positions FROM {self.name} 
+        WHERE id = {id};
+        """)
+        conn.commit()
+        cur.close()
+        print('[INFO] USER INFO DROPPED')
