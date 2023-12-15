61bb1841a935d41d2309d89dc2d8c4d4c8b1dcb3
ilyak <ilya.kustov.0606@mail.ru>
2023-12-15 17:22:10 +0500

    Pisun3

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -5,8 +5,8 @@ from lexicon.ru_lexicon import keyboard_lexicon
 
 
 menu_keyboard_builder = ReplyKeyboardBuilder()
-menu_buttons = [KeyboardButton(text=i, callback_data=j) for i, j in keyboard_lexicon['menu'].items()]
+menu_buttons = [KeyboardButton(text=keyboard_lexicon['menu'][i]) for i in range(len(keyboard_lexicon['menu']))]
 menu_keyboard_builder.add(*menu_buttons)
-menu_keyboard = menu_keyboard_builder.as_markup(one_time_keyboard=True, resize_keyboard=True)
+menu_keyboard = menu_keyboard_builder.as_markup(one_time_keyboard=True)
 
 
